c3f5f3bd6338892a168130d1b3904a1d051026dd
juajodha <89195607+juajodha@users.noreply.github.com>
2021-11-08 17:07:12 +0700

    Add files via upload

---
+++ ngalorngidul.js
@@ -0,0 +1,55 @@
+function capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+}
+function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+}
+function gen(){
+      var umr = ["22","23","24","25","26","27"];
+      var sym = ["😘","🥰","😍","💦","👙","😝","🥴","😜","🔞"];
+      var tag = ["✻","✼","✾","✿","❀","❁","❃","❇","❈","❉","❊","✣","✤"];
+      Rumr = capitalize(umr[getRandomInt(0, umr.length)])
+      Rsym = capitalize(sym[getRandomInt(0, sym.length)]);
+      Rtag = capitalize(tag[getRandomInt(0, tag.length)]);
+
+      var Kfr = [" Contactez-moi ici ⬇⬇"," Autres informations sur moi ⬇⬇"," Venez me parler sur mon profil ⬇⬇"," Chattez et appelez-moi ou trouvez-moi ⬇⬇"," En attente d'un partenaire sexuel 👄 Soyons coquins ⬇⬇"," Contactez ici ! 📩 Soyons méchants ⬇⬇"," Cliquez ici pour le sexe en direct ⬇⬇"," Voir plus ma photo et vidéo nue ⬇⬇"," Découvrez l'aperçu de ma dernière vidéo ⬇⬇"," J'ai besoin d'un rendez-vous, trouve-moi et appelle-moi ⬇⬇"," Si vous me cherchez, contactez-moi ici ! tout privé ⬇⬇"];
+
+      var Kus = [" Contact me here ⬇⬇"," Other Information About Me ⬇⬇"," Come Talk to Me on My Profile ⬇⬇"," Chat and call me or find me ⬇⬇"," Waiting for a sex partner 👄 Lets get naughty ⬇⬇"," Contact  here! 📩 Lets get naughty ⬇⬇"," Click here for the live sex ⬇⬇"," See more my nude photo and video ⬇⬇"," Check out the preview of my latest video ⬇⬇"," I need a date, Find and Call Me ⬇⬇"," If you are looking for me, contact me here! all private ⬇⬇"];
+
+      var Kit = [" Contattami qui ⬇⬇"," Altre informazioni su di me ⬇⬇"," Vieni a parlare con me sul mio profilo ⬇⬇"," Chatta e chiamami o trovami ⬇⬇"," Aspettando un partner sessuale 👄 Diventiamo cattivi ⬇⬇"," Contatta qui! 📩 Diventiamo cattivi ⬇⬇"," Clicca qui per il sesso dal vivo ⬇⬇"," Guarda di più la mia foto e il mio video di nudo ⬇⬇"," Guarda l'anteprima del mio ultimo video ⬇⬇"," Ho bisogno di un appuntamento, trova e chiamami ⬇⬇"," Se mi cerchi contattami qui! tutto privato ⬇⬇"];
+
+      var Kjp =[" こちらからご連絡ください ⬇⬇"," 私に関するその他の情報 ⬇⬇"," 私のプロフィールで私に話しに来てください ⬇⬇"," チャットして電話するか、見つけてください ⬇⬇"," セックスパートナーを待っています👄いたずらになりましょう ⬇⬇"," こちらからお問い合わせください！ 📩いたずらにしましょう ⬇⬇"," ライブセックスはこちら ⬇⬇"," 私のヌード写真とビデオをもっと見る ⬇⬇"," 私の最新のビデオのプレビューをチェックしてください ⬇⬇"," 日付が必要です、見つけて電話してください ⬇⬇"," あなたが私を探しているなら、ここで私に連絡してください！すべてプライベート ⬇⬇"];
+
+      var Kde =[" Kontaktiere mich hier ⬇⬇"," Weitere Informationen über mich ⬇⬇"," Komm, sprich mit mir auf meinem Profil ⬇⬇"," Chatte und ruf mich an oder finde mich ⬇⬇"," Warten auf einen Sexpartner 👄 Lass uns unartig werden ⬇⬇"," Kontakt hier! 📩 Lass uns unartig werden ⬇⬇"," Klicken Sie hier für den Live-Sex ⬇⬇"," Sehen Sie mehr mein Nacktfoto und Video ⬇⬇"," Schauen Sie sich die Vorschau meines neuesten Videos an ⬇⬇"," Ich brauche ein Date, Find and Call Me ⬇⬇"," Wenn Sie mich suchen, kontaktieren Sie mich hier! alles privat ⬇⬇"];
+
+      var Bfr = '⭐ ℕ𝕆𝕌𝕍𝔼𝔸𝕌 ℂ𝕆𝕄ℙ𝕋𝔼 ⭐\n' + Rtag + ' Seule ' + '|' + ' ' + Rumr + '\n' + Rtag + ' Je suis folle fille ' + Rsym + '\n' + Rtag + capitalize(Kfr[getRandomInt(0, Kfr.length)]);
+
+      var Bus = '⭐ ℕ𝔼𝕎 𝔸ℂℂ𝕆𝕌ℕ𝕋 ⭐\n' + Rtag + ' Single ' + '|' + ' ' + Rumr + '\n' + Rtag + ' Im crazy girl ' + Rsym + '\n' + Rtag + capitalize(Kus[getRandomInt(0, Kus.length)]);
+
+      var Bit = '⭐ ℕ𝕌𝕆𝕍𝕆 𝔸ℂℂ𝕆𝕌ℕ𝕋 ⭐\n' + Rtag + ' Singola ' + '|' + ' ' + Rumr + '\n' + Rtag + ' Sono pazza ragazza ' + Rsym + '\n' + Rtag + capitalize(Kit[getRandomInt(0, Kit.length)]);
+
+      var Bjp = '⭐ 新しいアカウント ⭐\n' + Rtag + ' 独身 ' + '|' + ' ' + Rumr + '\n' + Rtag + ' 私は狂った女の子です ' + Rsym + '\n' + Rtag + capitalize(Kjp[getRandomInt(0, Kjp.length)]);
+
+      var Bde = '⭐ ℕ𝔼𝕌𝔼𝕊 𝕂𝕆ℕ𝕋𝕆 ⭐\n' + Rtag + ' Einzel ' + '|' + ' ' + Rumr + '\n' + Rtag + ' Ich bin verrücktes Mädchen ' + Rsym + '\n' + Rtag + capitalize(Kde[getRandomInt(0, Kde.length)]);
+
+      if(document.getElementById('option').value == "FR") {
+            document.getElementById('result').innerHTML = Bfr
+      }
+      if(document.getElementById('option').value == "US") {
+            document.getElementById('result').innerHTML = Bus
+      }
+      if(document.getElementById('option').value == "IT") {
+            document.getElementById('result').innerHTML = Bit
+      }
+      if(document.getElementById('option').value == "JP") {
+            document.getElementById('result').innerHTML = Bjp
+      }
+      if(document.getElementById('option').value == "DE") {
+            document.getElementById('result').innerHTML = Bde
+      }
+}
+function copy() {
+      let textarea = document.getElementById("result");
+      textarea.select();
+      document.execCommand("copy");
+}
